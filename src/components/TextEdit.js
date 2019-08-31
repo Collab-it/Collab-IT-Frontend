e791daccb85b20172ce7c4d7cd2b1d6bc5fcf4ec
Piyush Mehta <me@piyushmehta.com>
2019-08-31 18:25:20 +0530

    textedit

---
+++ src/components/TextEdit.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class TextEdit extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Here's the TextEdit component.</h3>
+        <textarea
+          rows="10"
+          cols="50"
+          placeholder="Write something here..."
+        ></textarea>
+      </div>
+    );
+  }
+}
+export default TextEdit;
