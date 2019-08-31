328bacfd8172e0f8e666aed130efad7ed86639dc
Piyush Mehta <me@piyushmehta.com>
2019-08-31 18:25:41 +0530

    appjs

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h3>Here's the TextEdit component.</h3>
-        <textarea
-          rows="10"
-          cols="50"
-          placeholder="Write something here..."
-        ></textarea>
-      </div>
-    </div>
-  );
+  return <div className="App">text edit</div>;
 }
 
 export default App;
