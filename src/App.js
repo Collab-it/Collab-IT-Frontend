128c60418395057dee55ecadb012b825273df3ee
Piyush Mehta <me@piyushmehta.com>
2019-08-31 18:17:46 +0530

    front end static text edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>Here's the TextEdit component.</h3>
+        <textarea
+          rows="10"
+          cols="50"
+          placeholder="Write something here..."
+        ></textarea>
+      </div>
     </div>
   );
 }
