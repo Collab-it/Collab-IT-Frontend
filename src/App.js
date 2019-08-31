d159f710fa8d5bb04ab8b759ad302b001722683a
Piyush Mehta <me@piyushmehta.com>
2019-08-31 18:37:08 +0530

    yjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">text edit</div>;
+import TextEditContainer from "./container/TextEditContainer.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to TextEdit-App</h1>
+        </header>
+
+        <TextEditContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
